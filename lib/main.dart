b74483ce7aa87f8bd7285550dce99da081eecfb2
Uvais-Mohammad <uvaiskc55@gmail.com>
2022-10-23 20:16:44 +0530

    feat : firebase configured

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:reddit_clone/core/locale/localization.dart';
 import 'package:reddit_clone/features/auth/screens/login_screen.dart';
 import 'package:reddit_clone/theme/pallette.dart';
 
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Reddit Clone',
+      title: LocalizationStrings.appName ,
       theme: Pallette.darkModeAppTheme,
       home: const LoginScreen(),
     );
