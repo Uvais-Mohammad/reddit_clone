738987cc1db58fb6c1d569fe5829275a3c95fae8
Uvais-Mohammad <uvaiskc55@gmail.com>
2022-10-26 09:35:28 +0530

    Implemented fp dart

--- lib/features/auth/repository/auth_repository.dart
+++ lib/features/auth/repository/auth_repository.dart
@@ -1,10 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fpdart/fpdart.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:reddit_clone/core/constants/asset_manager.dart';
+import 'package:reddit_clone/core/constants/firebase_constants.dart';
+import 'package:reddit_clone/core/providers/failure.dart';
 import 'package:reddit_clone/core/providers/firebase_providers.dart';
+import 'package:reddit_clone/core/type_defs.dart';
+import 'package:reddit_clone/models/user_model.dart';
 
 final authRepositoryProvider = Provider<AuthRepository>(
   (ref) => AuthRepository(
@@ -31,7 +36,7 @@ class AuthRepository {
         _firebaseStorage = firebaseStorage,
         _googleSignIn = googleSignIn;
 
-  void googleSignIn() async {
+  FutureEither<UserModel> googleSignIn() async {
     try {
       final GoogleSignInAccount? googleSignInAccount =
           await _googleSignIn.signIn();
@@ -45,9 +50,39 @@ class AuthRepository {
       final UserCredential userCredential =
           await _firebaseAuth.signInWithCredential(credential);
 
-      debugPrint(userCredential.user?.email);
+      UserModel userModel;
+
+      if (userCredential.additionalUserInfo!.isNewUser) {
+        User user = userCredential.user!;
+        userModel = UserModel(
+          name: user.displayName ?? '',
+          profilePic: AssetManager.avatarDefault,
+          banner: AssetManager.bannerDefault,
+          uid: userCredential.user!.uid,
+          isAuthenticated: false,
+          karma: 0,
+          awards: [],
+        );
+
+        _users.doc(user.uid).set(userModel.toMap());
+      } else {
+        userModel = await _getUserData(userCredential.user!.uid).first;
+      }
+
+      return right(userModel);
+    } on FirebaseException catch (e) {
+      throw e.message!;
     } catch (e) {
-      debugPrint(e.toString());
+      return left(Failure(e.toString()));
     }
   }
+
+  CollectionReference get _users =>
+      _fireStore.collection(FirebaseConstants.usersCollection);
+
+  Stream<UserModel> _getUserData(String uid) => _users.doc(uid).snapshots().map(
+        (event) => UserModel.fromMap(
+          event.data() as Map<String, dynamic>,
+        ),
+      );
 }
