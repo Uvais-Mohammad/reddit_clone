6c02f0d0ecbbf88896c6e99f269000ba10989d32
Uvais-Mohammad <uvaiskc55@gmail.com>
2022-11-06 18:00:43 +0530

    routermaster added

---
+++ lib/core/routes/routes.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:reddit_clone/features/auth/screens/login_screen.dart';
+import 'package:routemaster/routemaster.dart';
+
+final routes = RouteMap(routes: {
+  '/': (_) => const MaterialPage(
+        child: LoginScreen(),
+      ),
+});
--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,10 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reddit_clone/core/locale/localization.dart';
-import 'package:reddit_clone/features/auth/screens/login_screen.dart';
+import 'package:reddit_clone/core/routes/routes.dart';
 import 'package:reddit_clone/firebase_options.dart';
 import 'package:reddit_clone/theme/pallette.dart';
+import 'package:routemaster/routemaster.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,11 +24,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       title: LocalizationStrings.appName,
       theme: Pallette.darkModeAppTheme,
-      home: const LoginScreen(),
+      routerDelegate: RoutemasterDelegate(routesBuilder: (context) => routes),
+      routeInformationParser: const RoutemasterParser(),
     );
   }
 }
